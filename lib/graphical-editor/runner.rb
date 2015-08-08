b7b767e74091fe5b7f906528e8e90d1ef94fa98f
Nikolai Berkoff <nikolai.berkoff@blacksquaremedia.com>
2015-08-08 11:05:58 +0100

    Link up bin runner

--- lib/graphical-editor/runner.rb
+++ lib/graphical-editor/runner.rb
@@ -2,7 +2,12 @@ module GraphicalEditor
   class Runner
     class << self
       def start
-
+        command = Command.new(nil)
+        loop do
+          print '>> '
+          line = gets.chomp!
+          command.route(line)
+        end
       end
     end
   end
